79cd8a0d8ede84ab8b879f78d438e2b0312e2b6f
hacker-bue-bom <95648036+hacker-bue-bom@users.noreply.github.com>
2022-05-24 19:09:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ErrorPage from '../views/ErrorPage.vue'
+//import adminView from '../views/adminView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'ErrorPage',
+    component: ErrorPage
+  },
+  {
+    path: '/carrinho',
+    name: 'CarrinhoCompras',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/CarrinhoCompras.vue')
+  },
+  {
+    path: '/adminView',
+    name: 'adminView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/adminView.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
